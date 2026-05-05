19503d1c6df8e806b6d24ff2524acc73d87d0f22
i57k <aakib.thasleem@edu.tbz.ch>
2026-05-05 12:29:22 +0200

    Fix typescript build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        imprint: resolve(__dirname, 'imprint.html'),
-        privacy: resolve(__dirname, 'privacy.html')
+        main: 'index.html',
+        imprint: 'imprint.html',
+        privacy: 'privacy.html'
       }
     }
   }
